c0b4f82928eac1d6d921bcf26098ccc773f23e7d
Nethala Nikhil <109343084+NethalaNikhil@users.noreply.github.com>
2023-12-04 15:38:25 +0530

    version-2

--- app.js
+++ app.js
@@ -1,35 +1,44 @@
-//jshint esversion:6
+// app.js
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use("view-engine", "ejs"); 
+const router = express.Router();
 
-app.get("/",function(req,res){
-    var today = new Date();
-    var day = today.getDay();
-    var whichday = " ";
-    switch (day)
-    {
-        case 1:
-            whichday = "monday";    
-        case 2:
-            whichday = "tuesday";
-        case 3:
-            whichday = "wednesday";
-        case 4:
-            whichday = "thursday";
-        case 5:
-            whichday = "friday";
-        case 6:
-            whichday ="saturday";
-        case 0:
-            whichday ="sunday";
-        
-    }
-    res.render("list",{day:whichday})
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  var today = new Date();
+  var day = today.getDay();
+  var whichday = " ";
+  switch (day) {
+    case 1:
+      whichday = "Monday";
+      break;
+    case 2:
+      whichday = "Tuesday";
+      break;
+    case 3:
+      whichday = "Wednesday";
+      break;
+    case 4:
+      whichday = "Thursday";
+      break;
+    case 5:
+      whichday = "Friday";
+      break;
+    case 6:
+      whichday = "Saturday";
+      break;
+    case 0:
+      whichday = "Sunday";
+      break;
+  }
+  res.render("list", { Day: whichday });
 });
 
-app.listen(3000,function(){
-    console.log("Server started on port 3000")
-})
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
+
+module.exports = router;

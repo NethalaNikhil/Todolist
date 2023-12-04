f9f8d7e91d27fa9f812a05a8ff8937e7e431efb8
Nethala Nikhil <109343084+NethalaNikhil@users.noreply.github.com>
2023-12-04 21:45:46 +0530

    Add files via upload
    changed var to let because of var acts as global

--- app.js
+++ app.js
@@ -3,38 +3,36 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+let items = ["buy food"];
 const router = express.Router();
 
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get("/", function (req, res) {
-  var today = new Date();
-  var day = today.getDay();
-  var whichday = " ";
-  switch (day) {
-    case 1:
-      whichday = "Monday";
-      break;
-    case 2:
-      whichday = "Tuesday";
-      break;
-    case 3:
-      whichday = "Wednesday";
-      break;
-    case 4:
-      whichday = "Thursday";
-      break;
-    case 5:
-      whichday = "Friday";
-      break;
-    case 6:
-      whichday = "Saturday";
-      break;
-    case 0:
-      whichday = "Sunday";
-      break;
-  }
-  res.render("list", { Day: whichday });
+  let today = new Date();
+  let day = today.getDay();
+  let options =
+  {
+    weekday:"long",//entire day of that week
+    date:"numeric",
+    month:"long"
+  };
+
+  let whichday = today.toLocaleDateString("en-US", options);
+  res.render("list", { Day: whichday ,newListItems: items});
+  
+
+  // server handle post request
+  app.post("/",function(req,res){
+    let item = req.body.newItems;
+    // console.log(item); to print in console
+    items.push(item);
+
+    res.redirect("/"); 
+
+  });
 });
 
 app.listen(3000, function () {
